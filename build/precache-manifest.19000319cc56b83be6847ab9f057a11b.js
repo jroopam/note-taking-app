5801d07d36694d56ae7852a0ee31557b98aa177d
Roopam <jroopam.792@gmail.com>
2022-04-17 11:28:58 +0530

    Create basic layout

---
+++ build/precache-manifest.19000319cc56b83be6847ab9f057a11b.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abb833c95099a6d4221613dbd3ae4a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b959ee2a6658c0d0299",
+    "url": "/static/js/2.235e61d4.chunk.js"
+  },
+  {
+    "revision": "6d4a67b9c6d598f3575a",
+    "url": "/static/js/main.bbee7dad.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
---
+++ src/components/Note.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+function Note(){
+    return <div className="note">
+        <h1>Heading</h1>
+        <p>This is a sample note. Nothing Here.</p>
+    </div>
+}
+
+export default Note
